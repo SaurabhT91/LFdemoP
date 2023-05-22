86cda55072403e8bdabbf89861e4b9d8c0a87adc
SAURABH91TIDGAM <saurabhtidgam1991@gmail.com>
2023-05-22 21:10:19 +0530

    adding javascript

---
+++ demo1/resources/js/user_page.js
@@ -0,0 +1,23 @@
+function deleteLeadById(id){
+  var deleteID=id;
+  fetch('http://localhost:7008/action_performed.php?deleteID='+deleteID, {'method': 'DELETE'})
+.then(res => res.text())
+.then(data => {
+    //alert(data)
+    location.href = "/user_page.php"
+})
+}
+
+function updateLeadById(id){
+
+  location.href = "/form.php?id="+id;
+
+}
+
+
+const signout = document.querySelector('#signOut')
+signout.addEventListener('click', () => location.href = "/logout.php")
+
+const addLead = document.querySelector('#new_Lead')
+addLead.addEventListener('click', () => location.href = "/form.php")
+
